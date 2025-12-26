a7589c2d560a99fbb5ec9715c36133666ac0f6a6
zen4215 <44k1.373@gmail.com>
2025-12-26 10:38:36 +0900

    add: krs3304r2 class declaration added

--- include/krs3304r2.hpp
+++ include/krs3304r2.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <IcsHardSerialClass.h>
+
+class KRS3304R2 {
+    public:
+    KRS3304R2(IcsHardSerialClass ics, const uint8_t id);
+    
+    void setDegree(float deg);
+
+    private:
+    IcsHardSerialClass m_ics;
+    const uint8_t      m_id;
+};
