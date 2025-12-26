944e1939a93e6eab1067c66333a5ca1765a6f8ac
zen4215 <44k1.373@gmail.com>
2025-12-26 10:57:13 +0900

    feat: krs3304r2

--- include/krs3304r2.hpp
+++ include/krs3304r2.hpp
@@ -1,14 +1,25 @@
 #pragma once
 
+#include <exception>
+
 #include <IcsHardSerialClass.h>
 
 class KRS3304R2 {
     public:
-    KRS3304R2(IcsHardSerialClass ics, const uint8_t id);
+    KRS3304R2(IcsHardSerialClass& ics, const uint8_t id);
     
     void setDegree(float deg);
 
     private:
-    IcsHardSerialClass m_ics;
-    const uint8_t      m_id;
+    static constexpr uint8_t MOTION_RANGE {135};
+
+    IcsHardSerialClass& m_ics;
+    const uint8_t       m_id;
 };
+
+KRS3304R2::KRS3304R2(IcsHardSerialClass& ics, const uint8_t id) : m_ics(ics), m_id(id) {}
+
+void KRS3304R2::setDegree(float deg) {
+    if(deg > MOTION_RANGE || deg < MOTION_RANGE) throw std::invalid_argument("krs3304r2::setDegree(): invaild degree, confirm motion range.");
+    m_ics.setPos(m_id, m_ics.degPos(deg));
+}
